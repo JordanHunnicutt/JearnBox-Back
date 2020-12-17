39b3a38ce6a5189ca85317e535744b0871e0a25e
sleepygibbs <gibbs041398@gmail.com>
2020-12-17 16:07:37 -0500

    Backend websockets are now running and interacting

--- src/main/java/com/dev/websocket/WebSocketConfig.java
+++ src/main/java/com/dev/websocket/WebSocketConfig.java
@@ -2,10 +2,12 @@ package com.dev.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@CrossOrigin("*")
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -18,6 +20,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("gs-guide-websocket").withSockJS();
+        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("http://localhost:3000").withSockJS();
     }
 }
